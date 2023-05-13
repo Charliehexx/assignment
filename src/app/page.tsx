9b0c56f54a4bcd2c387f8ad0ec2edd2ecaa36e0f
Aditya <adi2652002@gmail.com>
2023-05-13 22:53:36 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+  ercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
